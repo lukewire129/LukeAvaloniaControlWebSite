475225e2ca4dc276f83988087795c4380abab365
lukewire129 <lukewire129@gmail.com>
2024-10-20 01:22:19 +0900

    화면 업데이트

--- lukewireBlog/lukewireBlog/Domain/Main/Components/SideMenu.cs
+++ lukewireBlog/lukewireBlog/Domain/Main/Components/SideMenu.cs
@@ -10,20 +10,7 @@ public class SideMenu : ListBox
     {
         return new SideMenuItem();
     }
-    public static readonly AvaloniaProperty IsMenuVisibleProperty =
-        AvaloniaProperty.Register<SideMenu, bool>(nameof(IsMenuVisible));
-
-    public bool IsMenuVisible
-    {
-        get => (bool)GetValue(IsMenuVisibleProperty);
-        set => SetValue(IsMenuVisibleProperty, value);
-    }
     public SideMenu()
     {
-        this.SizeChanged += (s, e) =>
-        {
-            var width = e.NewSize.Width;
-            IsMenuVisible = width > 400;
-        };
     }
 }
